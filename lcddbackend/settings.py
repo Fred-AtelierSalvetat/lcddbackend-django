bad6c7a05724f75380366d493bcf435d194a1114
Frederic Salvetat <frederic.salvetat.developper@gmail.com>
2021-08-12 21:09:59 +0200

    Ass static file mng for deploiement

--- lcddbackend/settings.py
+++ lcddbackend/settings.py
@@ -133,8 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "statictmp",
 ]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

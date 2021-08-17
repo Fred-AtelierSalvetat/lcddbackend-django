be4f8e29d3edea594767e140ac91cbe0f9a20589
Frederic Salvetat <frederic.salvetat.developper@gmail.com>
2021-08-17 19:39:35 +0200

    Fix trailing endline issue in version

--- lcddbackend/settings.py
+++ lcddbackend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-API_VERSION = "0." + open("VERSION.TXT", "r").read()
+API_VERSION = "0." + str(int(open("VERSION.TXT", "r").read()))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
